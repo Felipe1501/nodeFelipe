ffa0026efa79587b7f7026a8d049afc6721313b7
Felipe1501 <felipebsantos1501@gmail.com>
2022-09-26 12:01:58 -0300

    fix: singleton

---
+++ exe03_singleton.js
@@ -0,0 +1,7 @@
+let num = 1;
+
+function exibProx() {
+    console.log(num++);
+}
+
+module.exports = {exibProx};
---
+++ exe03_teste.js
@@ -0,0 +1,7 @@
+const s1 = require('./exe03_singleton');
+const s2 = require('./exe03_singleton');
+
+s1.exibProx();
+s2.exibProx();
+s1.exibProx();
+s2.exibProx();
